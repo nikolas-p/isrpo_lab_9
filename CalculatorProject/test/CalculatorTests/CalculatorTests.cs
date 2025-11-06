2a5c944b7148872f119677fe225e2cbb27a35f59
nikolas-p <nikolajpereselkov67@gmail.com>
2025-11-06 22:12:50 +0300

    Initial commit

---
+++ CalculatorProject/test/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorLib;
+using Xunit;
+
+namespace CalculatorTests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            double result = calc.Add(5, 3);
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void Divide_ByZero_ShouldThrowException()
+        {
+            var calc = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calc.Divide(5, 0));
+        }
+    }
+}
